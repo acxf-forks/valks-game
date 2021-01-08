12edaa50600262b054a10ce889392a3ee902f69f
valkyrienyanko <sebastianbelle074@gmail.com>
2021-01-08 13:27:19 -0600

    Spherical Entity Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,31 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// The player is a temporary class to help with debugging.
 public class PlayerController : MonoBehaviour
 {
     public Transform planet;
+    private Planet planetScript;
     public float gravityForce = 1f;
+    private float radius;
     public float speed = 1f;
-    private float angle = 0f;
 
     private Rigidbody rb;
 
-    private Vector3 target = new Vector3(-100, 0, 0);
+    private Vector3 target;
+
+    private float playerHeight = 2;
 
     private void Awake()
     {
-        //rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        planetScript = planet.GetComponent<Planet>();
+    }
+
+    private Vector3 prevPos;
+
+    private void Start()
+    {
+        radius = planetScript.r;
+        
+        transform.position = new Vector3(0, radius + playerHeight / 2, 0);
+
+        target = new Vector3(-radius, 0, 0);
     }
 
     private void Update()
     {
         Vector3 gravityDir = (planet.position - transform.position).normalized;
-        //rb.AddForce(gravityDir * gravityForce);
-        //transform.up = -gravityDir;
 
-        var radius = 101;
-        Vector3 newPos = planet.position + new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
-        transform.position = newPos;
-        angle += 0.1f;
+        // Always stand on planet
+        transform.up = -gravityDir;
+
+        if (Vector3.Distance(transform.position, target) > ((playerHeight / 2) + 1))
+        {
+            // Planet Gravity
+            rb.AddForce(gravityDir * gravityForce);
+
+            // Move to Target
+            Vector3 targetDir = (target - transform.position).normalized;
+            rb.AddForce(targetDir * speed);
+        }
+        else 
+        {
+            rb.velocity = Vector3.zero;
+        }
     }
 }
