ef36502388f7fb6fa8277f77c3e903505657b537
valkyrienyanko <sebastianbelle074@gmail.com>
2021-01-22 23:34:14 -0600

    Validate Chunk Count

--- Assets/Scripts/Mesh/New/PlanetF.cs
+++ Assets/Scripts/Mesh/New/PlanetF.cs
@@ -14,6 +14,9 @@ public class PlanetF : MonoBehaviour
 
     private void OnValidate()
     {
+
+        planetSettings.chunks = ValidateChunkCount(planetSettings.chunks);
+
         GeneratePlanet();
     }
 
@@ -24,4 +27,13 @@ public class PlanetF : MonoBehaviour
 
         new PlanetMeshF().Create(planetSettings);
     }
+
+    /*!
+     * 4 triangles can be fit into 1 triangle. Likewise 4 "chunks" can be fit into
+     * a "base form face". Therefore the number of chunks should be a multiple of 4
+     * unless the chunk count is less than 4 then the chunk count should always be 1.
+     * 
+     * @return The number of chunks to generate per base face.
+     */
+    private int ValidateChunkCount(int chunks) => chunks < 4 ? 1 : Mathf.Max(4, chunks / 4 * 4);
 }
