588dc31893e12ce97b6e554e2e42b6b5a8d311b9
valkyrienyanko <sebastianbelle074@gmail.com>
2021-01-23 22:39:38 -0600

    Planet Mesh Face Chunks

--- Assets/Scripts/Mesh/New/PlanetMeshF.cs
+++ Assets/Scripts/Mesh/New/PlanetMeshF.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class PlanetMeshF
 {
-    private List<Vector3> vertices;
+    public static List<Vector3> baseFormVertices;
 
-    public void Create(PlanetSettingsF settings)
+    private PlanetSettingsF settings;
+    private PlanetF planet;
+
+    public void Create(PlanetF planet, PlanetSettingsF settings)
     {
+        this.planet = planet;
+
         var radius = settings.radius;
         var t = (1.0f + Mathf.Sqrt(5.0f)) / 2.0f;
 
-        vertices = new List<Vector3>()
+        // The base vertices that make up a base form icosahedron
+        baseFormVertices = new List<Vector3>()
         {
             new Vector3(-1, t, 0).normalized * radius,
             new Vector3(1, t, 0).normalized * radius ,
@@ -27,30 +33,49 @@ public class PlanetMeshF
             new Vector3(-t, 0, 1).normalized * radius
         };
 
-        GenerateChunks();
+        HandleBaseFormFaces();
     }
 
-    private void GenerateChunks() 
+    private void HandleBaseFormFaces()
     {
-        /*0, 5, 11,
-        0, 1, 5, 
-        0, 7, 1, 
-        0, 10, 7,
-        0, 11, 10
-        1, 9, 5, 
-        5, 4, 11,
-        11, 2, 10
-        10, 6, 7,
-        7, 8, 1, 
-        3, 4, 9, 
-        3, 2, 4, 
-        3, 6, 2, 
-        3, 8, 6, 
-        3, 9, 8, 
-        4, 5, 9, 
-        2, 11, 4,
-        6, 10, 2,
-        8, 7, 6, 
-        9, 1, 8, */
+        GenerateChunks(0, 11, 5);
+        GenerateChunks(0, 5, 1);
+        GenerateChunks(0, 1, 7);
+        GenerateChunks(0, 7, 10);
+        GenerateChunks(0, 10, 11);
+        GenerateChunks(1, 5, 9);
+        GenerateChunks(5, 11, 4);
+        GenerateChunks(11, 10, 2);
+        GenerateChunks(10, 7, 6);
+        GenerateChunks(7, 1, 8);
+        GenerateChunks(3, 9, 4);
+        GenerateChunks(3, 4, 2);
+        GenerateChunks(3, 2, 6);
+        GenerateChunks(3, 6, 8);
+        GenerateChunks(3, 8, 9);
+        GenerateChunks(4, 9, 5);
+        GenerateChunks(2, 4, 11);
+        GenerateChunks(6, 2, 10);
+        GenerateChunks(8, 6, 7);
+        GenerateChunks(9, 8, 1);
+    }
+
+    private void GenerateChunks(int a, int b, int c)
+    {
+        var numChunks = settings.chunks;
+        GenerateChunk(a, b, c);
+    }
+
+    private void GenerateChunk(int a, int b, int c) 
+    {
+        // Create chunk gameObject
+        var chunkObj = new GameObject();
+
+        // Set parent
+        chunkObj.transform.parent = planet.transform;
+
+        // Add PlanetMeshChunk script to chunk gameObject
+        var chunkScript = chunkObj.AddComponent<PlanetMeshChunkF>();
+        chunkScript.Create(a, b, c);
     }
 }
