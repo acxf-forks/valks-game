ea710dc511f073e58095efdaf13bf7b001e70692
valkyrienyanko <sebastianbelle074@gmail.com>
2021-01-23 23:24:18 -0600

    Add Material to Settings

--- Assets/Scripts/Mesh/New/PlanetSettingsF.cs
+++ Assets/Scripts/Mesh/New/PlanetSettingsF.cs
@@ -13,4 +13,7 @@ public class PlanetSettingsF
     [Tooltip("The number of chunks per base face. (Should be a multiple of 4)")]
     [Range(4, 32)]
     public int chunks = 4;
+
+    [Tooltip("The material of the planet.")]
+    public Material material;
 }
