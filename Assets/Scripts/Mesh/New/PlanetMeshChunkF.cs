9f63b1d804ba46fc48c31572565af5316a26078c
valkyrienyanko <sebastianbelle074@gmail.com>
2021-01-23 22:38:57 -0600

    Update PlanetMeshChunkF.cs

--- Assets/Scripts/Mesh/New/PlanetMeshChunkF.cs
+++ Assets/Scripts/Mesh/New/PlanetMeshChunkF.cs
@@ -8,8 +8,22 @@ public class PlanetMeshChunkF : MonoBehaviour
     private List<Vector3> vertices;
     private List<int> triangles;
 
-    public void Create() 
+    private Mesh mesh;
+
+    private static int count = 0;
+
+    public void Create(int a, int b, int c) 
     {
-        
+        count++;
+        gameObject.name = $"Chunk {count}";
+        var baseFormVertices = PlanetMeshF.baseFormVertices;
+        vertices = new List<Vector3> { baseFormVertices[a], baseFormVertices[b], baseFormVertices[c] };
+        triangles = new List<int>();
+        triangles.AddRange(new List<int> { 0, 1, 2 });
+
+        mesh = GetComponent<MeshFilter>().mesh;
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
     }
 }
